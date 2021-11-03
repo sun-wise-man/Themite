9ea08f8050c8a0ab3655fe01ac3a7b23c1f17b05
djkampus <71543444+djkampus@users.noreply.github.com>
2021-11-03 11:50:21 +0700

    entri script billboarding
    baru skript beluma ada asset

--- Assets/Resources/Scripts/Billboard.cs
+++ Assets/Resources/Scripts/Billboard.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+
+    private Camera mainCam;
+    public bool useStaticBillboard;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        if(!useStaticBillboard){//untuk aktivasi opsi billboarding
+            transform.LookAt(mainCam.transform);
+        }else{
+            transform.rotation = mainCam.transform.rotation;
+        }
         
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
 }
