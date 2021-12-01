bd60ff6ddf09c5fda5566c47cc4e979226b19968
djkampus <71543444+djkampus@users.noreply.github.com>
2021-12-01 19:13:52 +0700

    patrol.cs wait to test
    kodenya udah siap
    tapi masih harus dicoba dulu

--- Assets/Resources/Scripts/Patrol.cs
+++ Assets/Resources/Scripts/Patrol.cs
@@ -11,15 +11,28 @@ public class Patrol : MonoBehaviour
     public float speed;
     public Transform[] moveSpots;
     private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
     // Start is called before the first frame update
     void Start()
     {
+        waitTime = startWaitTime;
         randomSpot = Random.Range(0, moveSpots.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        if(vector2.Distance(transform.position, moveSpots[randomSpot].position < 0.2f)){
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 }
